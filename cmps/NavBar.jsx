9ee809cdf812f7c6195803eae6d872f108fec65e
Gal Rondel <gal31010@gmail.com>
2020-04-26 15:20:08 +0300

    v1

--- cmps/NavBar.jsx
+++ cmps/NavBar.jsx
@@ -8,13 +8,8 @@ export function NavBar(props) {
             <li><NavLink exact to='/'>Home</NavLink></li>
             <li><NavLink to='/about'>About</NavLink></li>
             <li><NavLink to='/book'>Books</NavLink></li>
-<<<<<<< HEAD
             <li><NavLink to='/notes'>Notes</NavLink></li>
-            <li><NavLink to='/book'>Email</NavLink></li>
-=======
-            <li><NavLink to='/book'>Nots</NavLink></li>
             <li><NavLink to='/email'>Email</NavLink></li>
->>>>>>> e67b2971f485cd596e1d6ab5fcb025716e597d10
             {/* <button onClick={() => {
             props.history.goBack();
         }}>➜</button> */}
