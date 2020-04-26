28bba4e475a6338c55c8f5f372dcfedb1b9bfa97
yuvaal12 <yuvaal9@gmail.com>
2020-04-26 10:42:49 +0300

    v0.01

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Home from './pages/Home.jsx'
+export class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1>Lets Play</h1>
+                </header>
+                <main>
+                    <Home />
+                </main>
+
+            </div>
+        )
+    }
+}
+
---
+++ pages/Home.jsx
@@ -0,0 +1,10 @@
+export default class Home extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <h2>Home Sweet Home</h2>
+            </section>
+        )
+    }
+}
