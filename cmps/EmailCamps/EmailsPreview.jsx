bf42e3f4da14d246f05997f60350d4a3eb84e0fe
yuvaal12 <yuvaal9@gmail.com>
2020-04-28 12:15:57 +0300

    v2 time working

--- cmps/EmailCamps/EmailsPreview.jsx
+++ cmps/EmailCamps/EmailsPreview.jsx
@@ -1,16 +1,42 @@
 const { Link } = ReactRouterDOM
+import emailService from '../../services/emailService.js'
 
-export default function EmailsPreview(props) {
-    const { email } = props
-    const isRead = (!email.isRead)? 'bold' : ''
-    return (
-        <Link className="emailLink" to={`/email/${email.id}`}>
-            <article className={`email-preview ${isRead}`}>
-                <div className="email-from">{email.from}</div>
-                <div className="email-subject">{email.subject}</div>
-                <div className="email-body">{email.body}</div>
-                <div className="email-sentAt">{email.sentAt}</div>
-            </article>
-        </Link>
-    )
+export default class EmailsPreview extends React.Component {
+
+    state = {
+        star: this.props.email.isStar
+    }
+
+    setStar = () => {
+        const { email } = this.props
+        emailService.saveMail(email.id, 'isStar', !email.isStar)
+        this.setState(prevState => ({ star: !prevState.star }))
+    }
+
+    render() {
+        const { email } = this.props
+        const isRead = (!email.isRead) ? 'bold' : ''
+        const classStart = (this.state.star) ? 'yellow' : ''
+
+        return (
+            <div className="line-container">
+                <button className={`email-star ${classStart}`} onClick={this.setStar}>✯</button>
+                <Link className="emailLink" to={`/email/${email.id}`}>
+                    <article className={`email-preview ${isRead}`}>
+
+                        <div className="email-from">{email.from}</div>
+                        <div className="email-subject">{email.subject}</div>
+                        <div className="email-body">{email.body}</div>
+                        <div className="email-sent-at">{email.sentAt}</div>
+                    </article>
+                </Link>
+                <div className="btn-mail">
+                    <a onClick={this.setStar}><img src="/assets/icons/email-icon.png" alt="" /></a>
+                    <a onClick={this.setStar}><img src="/assets/icons/trash.png" alt="" /></a>
+                </div>
+            </div>
+        )
+
+    }
+
 }
--- services/emailService.js
+++ services/emailService.js
@@ -3,31 +3,36 @@ import storageService from './storageService.js'
 const KEY_Email = 'emails';
 
 
-var emailIdx = 0
 var gEmails = storageService.load(KEY_Email);
 
 var gDefualtEmails = [
-    { id: utilService.makeId(), from: 'dani', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: utilService.makeId(), from: 'dani', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: utilService.makeId(), from: 'moshe', subject: 'how are you?', body: 'i just wanted to know how are you ?', isRead: true, sentAt: 1551133930594 },
-    { id: utilService.makeId(), from: 'moshe', subject: 'how are you?', body: 'i just wanted to know how are you ?', isRead: true, sentAt: 1551133930594 },
-    { id: utilService.makeId(), from: 'yuval', subject: 'tnx for your help', body: 'hi thank you very much', isRead: false, sentAt: 1551133930594 }
+    { id: utilService.makeId(), from: 'dani', subject: 'Wassap?', body: 'Pick up!', isRead: false, isStar: false, sentAt: timeNow() },
+    { id: utilService.makeId(), from: 'dani', subject: 'Wassap?', body: 'Pick up!', isRead: false, isStar: true, sentAt: timeNow() },
+    { id: utilService.makeId(), from: 'moshe', subject: 'how are you?', body: 'i just wanted to know how are you ?', isRead: true, isStar: false, sentAt: timeNow() },
+    { id: utilService.makeId(), from: 'moshe', subject: 'how are you?', body: 'i just wanted to know how are you ?', isRead: true, isStar: false, sentAt: timeNow() },
+    { id: utilService.makeId(), from: 'yuval', subject: 'tnx for your help', body: 'hi thank you very much', isRead: false, isStar: true, sentAt: timeNow() }
 ]
 
 
 export default {
     query,
     getEmailById,
-    removeEmail
+    removeEmail,
+    saveMail
+}
+
+function timeNow() {
+    var d = new Date(),
+        h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+        m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+    var time = h + ':' + m;
+
+    return time
 }
 
 function getEmailById(emailId) {
-    const email = gDefualtEmails.find(email => email.id === emailId);
+    const email = gEmails.find(email => email.id === emailId);
     return Promise.resolve(email)
-}
-
-function _getIdxById(emailId) {
-    return gDefualtEmails.findIndex(email => email.id === emailId)
 }
 
 function removeEmail(emailId) {
@@ -37,13 +42,66 @@ function removeEmail(emailId) {
     return Promise.resolve();
 }
 
+function _getIdxById(BookId) {
+    return gEmails.findIndex(email => email.id === emailId)
+}
+
+
+function getEmailById(emailId) {
+    const email = gEmails.find(email => email.id === emailId);
+    return Promise.resolve(email)
+}
+
+function saveMail(emailId, filed, value) {
+
+    getEmailById(emailId)
+        .then((email) => {
+            email[filed] = value
+            var idx = _getIdxById(emailId)
+            gEmails[idx] = email
+        })
+    storageService.store(KEY_Email, gEmails);
+}
+
+function addEmail(email) {
+    var newEmail = _createEmail(email)
+    gEmails.push(newEmail);
+    storageService.store(KEY_Email, gEmails);
+    return newEmail;
+}
+
+
+function _createEmail(newEmail) {
+
+    return {
+        id: utilService.makeId(),
+        from: 'dani',
+        subject: 'Wassap?',
+        body: 'Pick up!',
+        isRead: false,
+        isStar: false,
+        sentAt: 1551133930594,
+
+        title: newEmail.volumeInfo.title,
+        subtitle: newEmail.volumeInfo.subtitle,
+        authors: newEmail.volumeInfo.authors,
+        publishedDate: newEmail.volumeInfo.publishedDate,
+        description: newEmail.volumeInfo.description,
+        pageCount: newEmail.volumeInfo.pageCount,
+        categories: newEmail.volumeInfo.categories,
+        thumbnail: newEmail.volumeInfo.imageLinks.thumbnail,
+        language: newEmail.volumeInfo.language,
+        listPrice: pricing
+    };
+}
+
 function query(filterBy = null) {
 
     if (!gEmails) gEmails = gDefualtEmails;
     var emails = gEmails;
     if (!filterBy) return Promise.resolve(gEmails)
     else {
-        var { body } = filterBy
+        var { body, read, unread } = filterBy
         emails = gEmails.filter(email => email.body.toLowerCase().includes(body))
 
     }
