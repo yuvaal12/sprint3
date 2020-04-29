0f596227e9eeea2d8c5be22a7d41742eee98a628
Gal Rondel <gal31010@gmail.com>
2020-04-29 11:24:26 +0300

    v2.5

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,6 +3,10 @@ export default class Home extends React.Component {
     render() {
         return (
             <section>
+                <video autoPlay="autoPlay" loop="loop" muted className="start-video">
+                    <source src="../assets/video/coding.mp4"/>
+                    Not supported
+                </video>
                 <main className="home-Page">
                   <h1>Welcome to</h1>
                   <h2>Gal {'&'} Yuval</h2>
