2c5596197096683140c98530a0a66e438ccf7dbc
Gal Rondel <gal31010@gmail.com>
2020-04-29 11:32:44 +0300

    v2.7

--- cmps/NavBar.jsx
+++ cmps/NavBar.jsx
@@ -5,11 +5,11 @@ export function NavBar(props) {
     
     return <nav>
         <ul className="nav-bar-links flex">
-            <NavLink exact to='/'><li>Home</li></NavLink>
-            <NavLink to='/about'><li>About</li></NavLink>
-            <NavLink to='/book'><li>Books</li></NavLink>
-            <NavLink to='/notes'><li>Notes</li></NavLink>
-            <NavLink to='/email'><li>Email</li></NavLink>
+            <NavLink exact to='/' onClick={props.onToggle}><li>Home</li></NavLink>
+            <NavLink to='/about' onClick={props.onToggle}><li>About</li></NavLink>
+            <NavLink to='/book' onClick={props.onToggle}><li>Books</li></NavLink>
+            <NavLink to='/notes' onClick={props.onToggle}><li>Notes</li></NavLink>
+            <NavLink to='/email' onClick={props.onToggle}><li>Email</li></NavLink>
             {/* <button onClick={() => {
             props.history.goBack();
         }}>➜</button> */}
