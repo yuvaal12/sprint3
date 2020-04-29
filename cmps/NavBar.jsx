f1ed5932e11168b0a0fd7bb2f6dfc0d30f068bde
Gal Rondel <gal31010@gmail.com>
2020-04-29 11:37:13 +0300

    v2.8

--- cmps/NavBar.jsx
+++ cmps/NavBar.jsx
@@ -2,17 +2,17 @@ const { NavLink } = ReactRouterDOM
 
 
 export function NavBar(props) {
-    
+
     return <nav>
         <ul className="nav-bar-links flex">
-            <NavLink exact to='/' onClick={props.onToggle}><li>Home</li></NavLink>
-            <NavLink to='/about' onClick={props.onToggle}><li>About</li></NavLink>
-            <NavLink to='/book' onClick={props.onToggle}><li>Books</li></NavLink>
-            <NavLink to='/notes' onClick={props.onToggle}><li>Notes</li></NavLink>
-            <NavLink to='/email' onClick={props.onToggle}><li>Email</li></NavLink>
-            {/* <button onClick={() => {
-            props.history.goBack();
-        }}>➜</button> */}
+            <NavLink exact to='/'><li>Home</li></NavLink>
+            <NavLink to='/about' ><li>About</li></NavLink>
+            <NavLink to='/book' ><li>Books</li></NavLink>
+            <NavLink to='/notes' ><li>Notes</li></NavLink>
+            <NavLink to='/email'><li>Email</li></NavLink>
+            <button className="btn-back" onClick={() => {
+                props.history.goBack();
+            }}>➜</button>
         </ul>
 
     </nav>
